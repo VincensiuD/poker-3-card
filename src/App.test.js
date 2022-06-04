fce84ca474850bb965fff6c2b4288a2b2d380742
VincensiuD <spanishfrenchromanian@gmail.com>
2022-06-04 14:42:11 +1000

    testing flush and pairs

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,59 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+
+
+
+import { getPersonHand, getCardSuit, getCardValue, checkFlush,checkPairs } from './services/rankChecker';
+
+
+describe("Checking if player's hand is a flush", () => {
+    test("Returns true if player's hand is flush", () => {
+
+        let player1 = [ { rank:2, suit:"H"},  { rank:3, suit:"H"},  { rank:4, suit:"H"}];
+        let player2 = [ { rank:2, suit:"S"},  { rank:3, suit:"S"},  { rank:4, suit:"D"}]
+
+        expect(checkFlush(player1)).toBe(true);
+        expect(checkFlush(player2)).toBe(false);
+  });
+});
+
+
+describe("Checking if player's hand has pair", () => {
+  test("Returns type of pairs", () => {
+
+      // let player1 = [ { rank:2, suit:"H"},  { rank:3, suit:"H"},  { rank:4, suit:"H"}];
+      // let player2 = [ { rank:2, suit:"S"},  { rank:3, suit:"S"},  { rank:3, suit:"D"}];
+      // let player3 = [ { rank:2, suit:"S"},  { rank:2, suit:"C"},  { rank:2, suit:"D"}];
+      // let player4 = [ { rank:2, suit:"S"},  { rank:3, suit:"S"},  { rank:10, suit:"D"}, {rank:10, suit:"H"}, {rank:10, suit:"C"}];
+      // let player5 = [ { rank:4, suit:"S"},  { rank:4, suit:"S"},  { rank:4, suit:"D"}, {rank:10, suit:"H"}, {rank:10, suit:"C"}];
+      // let player6 = [ { rank:2, suit:"S"},  { rank:10, suit:"S"},  { rank:10, suit:"D"}, {rank:10, suit:"H"}, {rank:10, suit:"C"}];
+
+      // expect(checkPairs(player1)).toBe("NP");
+      // expect(checkPairs(player2)).toBe("SP");
+      // expect(checkPairs(player3)).toBe("3K");
+      // expect(checkPairs(player4)).toBe("2P");
+      // expect(checkPairs(player5)).toBe("FH");
+      // expect(checkPairs(player6)).toBe("4K");
+
+
+      let p1 = [11,10,9];
+      let p2 = [1,2,5];
+      let p3 = [1,2,14,33,44,48];
+      let p4 = [3,4,5,6,23,35];
+      let p5 = [17,18,33,34,35,19];
+      let p6 = [33,34,35,36,41,50];
+      expect(checkPairs(p1)).toBe("3K");
+      expect(checkPairs(p2)).toBe("SP");
+      expect(checkPairs(p3)).toBe("SP");
+    //  expect(checkPairs(p4)).toBe(-1);
+      expect(checkPairs(p5)).toBe("FH");
+      expect(checkPairs(p6)).toBe("4K");
+});
 });
--- src/services/rankChecker.js
+++ src/services/rankChecker.js
@@ -5,7 +5,7 @@ export function getCardValue(cardNumber){
 
     let cardCode = (cardDictionary[cardNumber]);
   
-    return cardCode.value;
+    return cardCode.rank;
   
   }
 
@@ -25,3 +25,77 @@ export function getCardValue(cardNumber){
     return cardArray;
   }
 
+  export function getHandRank(arr){
+
+}
+
+export function checkFlush(arr){
+
+    let disparity = 0;
+
+        for (let j = 0; j < arr.length - 1; j += 1) {
+            if ((arr[j].suit !== arr[j + 1].suit )&&arr.length === 3)  {
+              return false;
+            }
+            else if((arr[j].suit !== arr[j + 1].suit )){
+                disparity++;
+            }
+            if(disparity>1)
+            {
+                return false;
+            }
+        }       
+        
+        return true;
+    }
+   
+export function checkPairs(arr){
+
+    let pairsDictionaryObject = {};
+
+
+    for(let i = 0; i < arr.length; i++){
+
+        let cardRank = getCardValue(arr[i]);
+    if(!pairsDictionaryObject.hasOwnProperty(cardRank)){
+        pairsDictionaryObject[cardRank] = 0;
+    }
+        pairsDictionaryObject[cardRank] += 1;
+    }
+
+    const dictLength = Object.keys(pairsDictionaryObject).length;
+
+    if(dictLength === arr.length){
+        return "NP";
+    }
+
+    if(dictLength === arr.length-1){
+        return "SP";
+    }
+
+    switch(dictLength){
+
+        case 1:
+            return "3K";
+
+        case 2:
+
+            if(pairsDictionaryObject.includes(4)){
+            return "4K";
+            }
+            return "FH";
+        
+        case 3:
+            if(pairsDictionaryObject.includes(3)){
+                return "3K";
+            }
+                return "2P"
+
+        default:
+           
+            return -1;
+
+    }
+
+  
+}
